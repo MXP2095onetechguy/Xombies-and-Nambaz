438e26b36cf7ccb63e5375f4e7bf1bfd706b96ca
MXPServer 20953 onetechguy <73059135+MXP2095onetechguy@users.noreply.github.com>
2021-06-10 12:15:57 +0700

    second version

--- Gam.cpp
+++ Gam.cpp
@@ -3,6 +3,10 @@
 #include <exception>
 #include <thread>
 #include <fstream>
+#include <chrono>
+#include <future>
+#include <vector>
+#include <csignal>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/generator_iterator.hpp>
 #include <boost/random.hpp>
@@ -17,6 +21,10 @@ using namespace boost::random;
 
 typedef independent_bits_engine<mt19937, 256, cpp_int> generator_type;
 generator_type gen(time(NULL));
+
+string* blank = new string("");
+string title = "Xombies and Nambaz";
+bool showstat = true;
 
 
 // class
@@ -40,9 +48,76 @@ int randint(int min, int max) { // new randomizer
     return res;
 }
 
+void keyboardinterruptsignal(int signum) {
+    cout << endl << "Keyboard interrupt dectected, exiting with code " << signum << ".";
+
+    // cleanup and close up stuff here  
+    // terminate program  
+
+    exit(signum);
+}
+
+void programtermsignal(int signum) {
+    cout << endl << "Program either did an illegal instruction or Another program sent a termination request to this program, exiting with code " << signum << ".";
+
+    exit(signum);
+}
+
+static void show_usage(string name)
+{
+    std::cout << "Usage: " << name << " <option(s)> SOURCES\n"
+        << "Options:\n"
+        << "\t-h,--help\t\tShow this help message\n"
+        << "\t-ns, --NOSTAT\t\tNo stat writting prompt\n"
+        << std::endl;
+}
+
 
 // main boi
 int main(int argc, char* argv[]){
-    throw ASRTBE;
+    beep(500, 500);
+    signal(SIGABRT, keyboardinterruptsignal);
+    signal(SIGINT, keyboardinterruptsignal);
+    signal(SIGILL, programtermsignal);
+    signal(SIGTERM, programtermsignal);
+
+#ifdef _WIN32
+    system("color 02");
+#endif
+
+    if (argc < 2)
+    {
+
+    }
+    else {
+        std::vector <std::string> sources;
+        std::string destination;
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+            if ((arg == "-h") || (arg == "--help")) {
+                show_usage(argv[0]);
+                return 0;
+            }
+            else if ((arg == "-ns") || (arg == "--NOSTAT"))
+            {
+                showstat = false;
+            }
+            else {
+                sources.push_back(argv[i]);
+            }
+        }
+    }
+
+
+
+    cout << "\033]2;" << title << "\007" << "Welcome to " << title << endl << "Press enter to get started. Type [EXIT or exit] to exit," << endl << "maybe do a keyboard interrupt anytime you want to also exit";
+    string shouldiexit = "";
+    getline(cin, shouldiexit);
+    shouldiexit = *blank;
+    if(shouldiexit == "exit" || shouldiexit == "EXIT"){
+        return 0;
+    }
+    delete blank;
+
     return 0;
 }
